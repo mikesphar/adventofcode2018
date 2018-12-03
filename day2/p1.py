2c1d1268d6be409b4b50182fc5442525da6285a8
Michael Sphar <mikesphar@users.noreply.github.com>
2018-12-03 11:54:16 -0600

    Now with more Class!

--- day2/p1.py
+++ day2/p1.py
@@ -4,33 +4,24 @@ from collections import Counter
 import fileinput
 
 boxes = list()
-twice_count = 0
-thrice_count = 0
 
 class Box:
+    twice = 0
+    thrice = 0
+
     def __init__(self, id):
         self.id = id
-        self.twice = False
-        self.thrice = False
 
         character_counts = Counter(id)
 
         if 2 in character_counts.values():
-            self.twice = True
+            Box.twice += 1
 
         if 3 in character_counts.values():
-            self.thrice = True
+            Box.thrice += 1
 
 for box_id in fileinput.input():
     boxes.append(Box(box_id))
 
-for box in boxes:
-    if box.twice:
-        twice_count += 1
-    if box.thrice:
-        thrice_count += 1
-
-print "Twice_count: ", twice_count
-print "Thrice_count: ", thrice_count
-print "Checksum: ", twice_count * thrice_count
+print "Checksum: ", Box.twice * Box.thrice
 
